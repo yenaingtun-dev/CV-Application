81159db24d78a5895cdeffe86746a9d93687906a
Ye Naing Tun <yenaingtun425@gmail.com>
2023-12-19 20:55:36 +0630

    updated applicaton

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,16 +1,22 @@
+import { FaEnvelope } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+import { FaLocationArrow } from "react-icons/fa";
 function Application() {
     return (
         <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
             <div className="p-10 text-center bg-red-300">
                 <h1 className="text-xl uppercase">John Doe</h1>
                 <div className="flex items-center justify-center">
-                    <div>
+                    <div className="flex items-center">
+                        <FaEnvelope />
                         <p>mail@mail.com</p>
                     </div>
-                    <div>
+                    <div className="flex items-center">
+                        <FaPhone />
                         <p>+44 3245 5521 5521</p>
                     </div>
-                    <div>
+                    <div className="flex items-center">
+                        <FaLocationArrow />
                         <p>London, UK</p>
                     </div>
                 </div>
