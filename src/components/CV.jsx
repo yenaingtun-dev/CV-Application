afbe6b09691e0ab5272f637de3fdae0428bc1831
Ye Naing Tun <yenaingtun425@gmail.com>
2024-06-06 20:51:51 +0630

    updated form

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,51 +1,105 @@
-import Application from "./Application";
-import Form from "./Form";
+import React from "react";
 import { useState } from "react";
 
-function CV() {
-    const [firstName, setFirstName] = useState("John");
-    const [lastName, setLastName] = useState("Doe");
-    // const [formData, setFormData] = useState({
-    //     firstName: "John",
-    //     lastName: "Doe",
-    //     position: "Web Developer",
-    //     email: "mail@mail.com",
-    //     phone: "+44 3245 5521 5521",
-    //     location: "London, UK",
-    //     jobPosition: "Web Developer",
-    //     companyName: "Myint Thu Kha Nadi Comapny",
-    //     startDate: "2022",
-    //     endDate: "2023",
-    //     companyLocation: "yangon",
-    //     jobDescription:
-    //         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, aperiam.",
-    //     jobLists: "Lorem ipsum dolor sit amet.",
-    //     major: "Botany",
-    //     university: "Dagon University Distance Education",
-    //     universityStartDate: "2017",
-    //     universityEndDate: "2019",
-    //     universityLocation: "yangon",
-    //     universityDescription:
-    //         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, aperiam.",
-    //     skill1: "HTML",
-    //     skill2: "CSS",
-    //     skill3: "JavaScript",
-    //     skill4: "ReactJS",
-    //     skill5: "PHP",
-    //     skill6: "Laravel",
-    // });
+const CV = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-    const handleNameChange = (nameType, value) => {
-        setNames((prevNames) => ({ ...prevNames, [nameType]: value }));
-    };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    return (
-        <section className="bg-white">
-            <div className="lg:grid lg:grid-cols-12">
-                {/* <Application firstName={firstName} lastName={lastName} />
-                <Form setFirstName={setFirstName} setLastName={setLastName} /> */}
+  return (
+    <section className="bg-gray-100">
+      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+          <div className="rounded-lg bg-white p-8 shadow-lg space-y-4 lg:col-span-3 lg:p-12">
+            <div>
+              <label className="sr-only" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="Name"
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name || ''}
+                onChange={handleInputChange}
+              />
             </div>
-        </section>
-    );
-}
+
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+              <div>
+                <label className="sr-only" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                  placeholder="Email address"
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email || ''}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div>
+                <label className="sr-only" htmlFor="phone">
+                  Phone
+                </label>
+                <input
+                  className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                  placeholder="Phone Number"
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  value={formData.phone || ''}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label className="sr-only" htmlFor="message">
+                Message
+              </label>
+
+              <textarea
+                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                placeholder="Message"
+                rows="8"
+                id="message"
+                name="message"
+                value={formData.message || ''}
+                onChange={handleInputChange}
+              ></textarea>
+            </div>
+
+            <div className="mt-4">
+              <button
+                type="submit"
+                className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+              >
+                Send Enquiry
+              </button>
+            </div>
+          </div>
+
+          <div className="lg:col-span-2 lg:py-12">
+            <p className="max-w-xl text-lg">Name: {formData.name}</p>
+            <p className="max-w-xl text-lg">Email: {formData.email}</p>
+            <p className="max-w-xl text-lg">Phone: {formData.phone}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default CV;
