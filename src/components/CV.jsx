8666f3ead5824115db692833fe9b3ddf7f575c71
Ye Naing Tun <yenaingtun425@gmail.com>
2024-06-09 15:26:22 +0630

    updated form

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -7,6 +7,7 @@ const CV = () => {
     lastName: "",
     email: "",
     phone: "",
+    dateOfBirth: "",
     address: "",
   });
 
@@ -18,7 +19,8 @@ const CV = () => {
   return (
     <section className="bg-gray-100">
       <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-5">
+          {/* form */}
           <div className="rounded-lg bg-white p-8 shadow-lg space-y-4 lg:col-span-3 lg:p-12">
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
               <div>
@@ -31,7 +33,7 @@ const CV = () => {
                   type="text"
                   id="firstName"
                   name="firstName"
-                  value={formData.firstName || ''}
+                  value={formData.firstName || ""}
                   onChange={handleInputChange}
                 />
               </div>
@@ -46,7 +48,7 @@ const CV = () => {
                   type="text"
                   id="lastName"
                   name="lastName"
-                  value={formData.lastName || ''}
+                  value={formData.lastName || ""}
                   onChange={handleInputChange}
                 />
               </div>
@@ -63,7 +65,7 @@ const CV = () => {
                   type="email"
                   id="email"
                   name="email"
-                  value={formData.email || ''}
+                  value={formData.email || ""}
                   onChange={handleInputChange}
                 />
               </div>
@@ -78,13 +80,27 @@ const CV = () => {
                   type="tel"
                   id="phone"
                   name="phone"
-                  value={formData.phone || ''}
+                  value={formData.phone || ""}
                   onChange={handleInputChange}
                 />
               </div>
             </div>
 
-            <div className="grid grid-cols-1 gap-4">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+              <div>
+                <label className="sr-only" htmlFor="dateOfBirth">
+                  Date Of Birth
+                </label>
+                <input
+                  className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                  placeholder="Date of Birth"
+                  type="text"
+                  id="dateOfBirth"
+                  name="dateOfBirth"
+                  value={formData.dateOfBirth || ""}
+                  onChange={handleInputChange}
+                />
+              </div>
               <div>
                 <label className="sr-only" htmlFor="address">
                   Address
@@ -95,7 +111,7 @@ const CV = () => {
                   type="text"
                   id="address"
                   name="address"
-                  value={formData.address || ''}
+                  value={formData.address || ""}
                   onChange={handleInputChange}
                 />
               </div>
@@ -110,12 +126,34 @@ const CV = () => {
               </button>
             </div>
           </div>
-
-          <div className="lg:col-span-2 lg:py-12">
-            <p className="max-w-xl text-lg">Name: {formData.firstName} {formData.lastName}</p>
-            <p className="max-w-xl text-lg">Email: {formData.email}</p>
-            <p className="max-w-xl text-lg">Phone: {formData.phone}</p>
-            <p className="max-w-xl text-lg">Address: {formData.address}</p>
+          {/* card */}
+          <div className="rounded-lg bg-white p-8 space-y-2 shadown-lg lg:col-span-2 lg:py-12">
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+              <div className="max-w-max">
+                <p className="font-bold">Name:</p>
+                <p>{formData.firstName} {formData.lastName}</p>
+              </div>
+              <div className="max-w-max">
+                <p className="font-bold">Email:</p>
+                <p>{formData.email}</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+              <div className="max-w-max">
+                <p className="font-bold">Phone:</p>
+                <p>{formData.phone}</p>
+              </div>
+              <div className="max-w-max">
+                <p className="font-bold">DateOfBirth:</p>
+                <p>{formData.dateOfBirth}</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 gap-4">
+              <div className="max-w-max">
+                <p className="font-bold">Address:</p>
+                <p>{formData.address}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
